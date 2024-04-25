6a4d8a206addf18444f162d431c566ebaba38ce9
patrik.cermak <patrik.cermak@VT2-08>
2024-04-25 13:11:16 +0200

    Přidejte soubory projektu.

---
+++ Bossfighter/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bossfighter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string name { get; set; }
+        Dungeon dungeon = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            if(name_input.Text != "")
+            {
+                name = name_input.Text;
+            }
+            else
+            {
+                name = "Keqing";
+            }
+            this.Close();
+            dungeon.Show();
+        }
+
+    }
+}
